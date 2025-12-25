0028b0e221cf1c0ab5a7130e9b00900f41cb4258
kaskovaarina <kaskovaarina3@gmail.com>
2025-12-25 16:17:58 +0300

    Update GoodsViewModel.kt

--- app/src/main/java/com/example/lyceum_saturday10_2025/features/goods/presentation/GoodsViewModel.kt
+++ app/src/main/java/com/example/lyceum_saturday10_2025/features/goods/presentation/GoodsViewModel.kt
@@ -59,7 +59,7 @@ class GoodsViewModel : ViewModel() {
                     name = name,
                     description = description,
                     rating = 5
-                    // Без imageUrl в базе
+                
                 )
             )
             _state.value = GoodsUiState(goodsList)
@@ -68,7 +68,7 @@ class GoodsViewModel : ViewModel() {
 
     fun deleteGood(goodId: Long) {
         viewModelScope.launch {
-            // Просто удаляем из списка в UI
+            
             val goodsList = state.value.items.filterNot { it.id == goodId }
             _state.value = GoodsUiState(goodsList)
         }
